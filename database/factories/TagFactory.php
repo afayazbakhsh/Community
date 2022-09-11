0ee7654de63ad550ade3ca2d6b993f7b5fc09266
AmirFayaz <Amirfayazfanavar>
2022-09-12 03:58:08 +0430

    feat: store record for test tag table

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+use App\Models\Community;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
+ */
+class TagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+
+
+        foreach(Post::all() as $post){
+
+            $post->tags()->create([
+                'name' => fake()->name(),
+            ]);
+        }
+
+        foreach(Community::all() as $community){
+
+            $community->tags()->create([
+                'name' => fake()->name(),
+            ]);
+        }
+
+    }
+}
